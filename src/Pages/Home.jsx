4e0deae8767d22a2356f7cc21d6ec36fa4cfa968
Kenneth-Parker <kennethkavon@gmail.com>
2023-11-21 21:16:53 -0500

    home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,10 +3,13 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="Home">
-      <h2>Want A Coat 👋!</h2>
-      <h3>
-        Welcome To The COATS 2 COATS App!
-      </h3>
+
+      <div className="home-container">
+        <h2>Want A Coat 👋!</h2>
+        <h3>
+          Welcome To The COATS 2 COATS App!
+        </h3>
+      </div>
     </div>
   );
 }
