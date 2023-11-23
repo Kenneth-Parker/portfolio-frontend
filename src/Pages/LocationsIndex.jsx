9cb0c02c363dcd349d757dda3d794763c85c7c2e
Kenneth-Parker <kennethkavon@gmail.com>
2023-11-23 07:04:45 -0500

    locations tab added

---
+++ src/Pages/LocationsIndex.jsx
@@ -0,0 +1,14 @@
+import LocationList from "../Components/Locations";
+import './LocationsIndex.css';
+
+
+function LocIndex() {
+  return (
+    <div className="locIndex">
+      <h2>Visit Our Locations</h2>
+      <LocationList />
+    </div>
+  );
+}
+
+export default LocIndex;
