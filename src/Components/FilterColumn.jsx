a8c7b082d2655366628646227392b7522ff22708
Kenneth-Parker <kennethkavon@gmail.com>
2023-12-02 04:27:16 -0500

    filter

---
+++ src/Components/FilterColumn.jsx
@@ -0,0 +1,60 @@
+// FilterColumn.js
+import React, { useState } from 'react';
+import './FilterColumn.css';
+
+const FilterColumn = ({ applyFilter }) => {
+  const [selectedFilter, setSelectedFilter] = useState('');
+  const [selectedLocation, setSelectedLocation] = useState('');
+
+  const handleFilterChange = (value) => {
+    setSelectedFilter(value);
+    applyFilter(value, selectedLocation);
+  };
+
+  const handleLocationChange = (locationId) => {
+    setSelectedLocation(locationId);
+    applyFilter(selectedFilter, locationId);
+  };
+
+  const handleClearFilter = () => {
+    setSelectedFilter('');
+    setSelectedLocation('');
+    applyFilter('', '');
+  };
+
+  return (
+    <div className="filter-column">
+      <h2>FILTER</h2>
+      
+      <label>
+        Filter by Availability:
+        <select
+          value={selectedFilter}
+          onChange={(e) => handleFilterChange(e.target.value)}
+        >
+          <option value="">All Coats</option>
+          <option value="available">Available Coats</option>
+          <option value="unavailable">Unavailable Coats</option>
+        </select>
+      </label>
+
+      <label>
+        Filter by Location:
+        <select
+          value={selectedLocation}
+          onChange={(e) => handleLocationChange(e.target.value)}
+        >
+          <option value="">All Locations</option>
+          <option value="1">New York, NY</option>
+          <option value="2">Bronx, NY</option>
+          <option value="3">Brooklyn, NY</option>
+          {/* Add more location options as needed */}
+        </select>
+      </label>
+
+      <button className="filter-button" onClick={handleClearFilter}>Clear Filter</button>
+    </div>
+  );
+}
+
+export default FilterColumn;
